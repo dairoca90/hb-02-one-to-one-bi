09ba7e9ef2d1d2310729c422c84ac6aaf0056ae0
Danivan Rosales <dairoca90@gmail.com>
2020-01-27 11:45:11 -0600

    update libraries

---
+++ src/com/luv2code/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,61 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class DeleteInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory 	= new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		//create a session
+		Session session = factory.getCurrentSession();
+		try {
+			
+			
+			//start the transaction
+			session.beginTransaction();
+			//get the instructor detail object
+			int theId = 3;
+			InstructorDetail tempInstructorDetail =
+					session.get(InstructorDetail.class, theId);
+			
+			//print the instructor detail objet
+			System.out.println("Temp instructor detail : " + tempInstructorDetail);
+			//print the associated instructor object
+			System.out.println("Temp instructor: "+ tempInstructorDetail.getInstructor());
+			//now lets delete de instructor detail
+			System.out.println("Deleting instructorDetail : " 
+					+ tempInstructorDetail);
+			
+			//remove the associated object reference
+			//break bi-directional link
+			tempInstructorDetail.getInstructor().setInstructorDatail(null);
+			
+			
+			session.delete(tempInstructorDetail);
+			
+			//commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done ... ");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}finally {
+			
+			session.close();
+			factory.close();
+		}
+	}	
+
+}
